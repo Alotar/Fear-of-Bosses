712d8dad9b69f0f0556705076a21acfe9ca0d42c
Balys Dapkūnas <alotar@gmail.com>
2010-12-19 19:22:11 +0200

    Added exception support for server.

--- cpp/server/src/main.cpp
+++ cpp/server/src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 
 #include "manager.h"
+#include "network/networkexception.h"
 
 int main(int argc, char *argv[]) {
   std::cout << "Loading server. Port is 30123" << std::endl;
-  fob::server::Manager manager(30123);
-  manager.Run();
+
+  try {
+    fob::server::Manager manager(30123);
+    manager.Run();
+  }
+  catch (fob::server::network::NetworkException &e) {
+    std::cout << "Server quit with the following error: " << e.message() << std::endl;
+    return 1;
+  }
   return 0;
 }
